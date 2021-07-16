3d93c7aac734307091180b4acc0f5b3c19da81da
Marcio Fernandes <farciomernandes@gmail.com>
2021-07-16 10:14:07 -0300

    adiciona configuracao para a documentacao da api apresentar o parametro Authorization nos header

--- src/main/java/br/alura/forum/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/alura/forum/config/swagger/SwaggerConfigurations.java
@@ -3,12 +3,16 @@ package br.alura.forum.config.swagger;
 import br.alura.forum.model.Usuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-@Configuration
+import java.util.Arrays;
+
+@Configuration //configurações de como o swagger deve analisar a API
 public class SwaggerConfigurations {
 
     @Bean
@@ -22,6 +26,17 @@ public class SwaggerConfigurations {
                 .build()
                 //Ignorar todas as url que trabalham com a classe Usuario
                 //porque na classe tem a senha, e ela pode começar a aparecer
-                .ignoredParameterTypes(Usuario.class);
+                .ignoredParameterTypes(Usuario.class)
+                //adiciona um parametro a todos os endpoints
+                .globalOperationParameters(
+                        Arrays.asList(
+                                new ParameterBuilder()
+                                        //o nome do parametro e descrições
+                                        .name("Authorization")
+                                        .description("Header para Token JWT")
+                                        .modelRef(new ModelRef("string"))
+                                        .parameterType("header")
+                                        .required(false)
+                                        .build()));
     }
 }
