56e12813537dad5377281d4d413f96174add40cf
Marcio Fernandes <farciomernandes@gmail.com>
2021-07-19 09:28:11 -0300

    desfazendo configurações de mudanca de build

--- src/main/java/br/alura/forum/ForumApplication.java
+++ src/main/java/br/alura/forum/ForumApplication.java
@@ -2,8 +2,6 @@ package br.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,16 +10,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport //Permite pegar dos parametros da requisicao, campos de paginacao e ordenacao 'TopicosController'
 @EnableCaching //Habilita cache na aplicação
 @EnableSwagger2 //Habilita o Swagger para documentação automatica da api
-public class ForumApplication extends SpringBootServletInitializer {
+public class ForumApplication{
     //O SpringBootServletInitializer: Torna possível configurar o Servlet dentro do servidor 'tomcat'
 
     public static void main(String[] args) {
         SpringApplication.run(ForumApplication.class, args);
     }
 
-    //sobrescreve esse método
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(ForumApplication.class);
-    }
 }
